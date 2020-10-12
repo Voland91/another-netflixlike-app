b4f7629c9a4ffbdff1f703cceb719abf63f63317
Mateusz Butkiewicz <butkiewicz.mat@gmail.com>
2020-10-12 11:45:26 +0200

    Updates

---
+++ precache-manifest.e33d9fb5f9ede61d16beec813687b8be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "beea15d9a9cf72601cb3",
+    "url": "/another-netflixlike-app/static/js/main.4f18fb13.chunk.js"
+  },
+  {
+    "revision": "ca1f44ffcf4fad4c2f04",
+    "url": "/another-netflixlike-app/static/js/runtime~main.b795088d.js"
+  },
+  {
+    "revision": "fdd82c299cb604c00af9",
+    "url": "/another-netflixlike-app/static/js/2.d3c5ed20.chunk.js"
+  },
+  {
+    "revision": "659d7dab6a7a25231fdea0320c3fcc0c",
+    "url": "/another-netflixlike-app/static/media/logo.659d7dab.svg"
+  },
+  {
+    "revision": "90ffe448f06b91e5de21d334c76880df",
+    "url": "/another-netflixlike-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/another-netflixlike-app/precache-manifest.e33d9fb5f9ede61d16beec813687b8be.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/another-netflixlike-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
